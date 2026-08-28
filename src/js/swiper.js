d3c587d0f3f124486170b53a34878f31c38530b4
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2026-08-28 13:30:50 +0400

    Add SCSS styles for technology FAQ component and its slider

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -22,3 +22,25 @@ if (technologyFeaturesSlider) {
     rewind: true,
   });
 }
+
+/** @type {HTMLDivElement} */
+const technologyFaqSlider = document.querySelector(".technology-faq-slider");
+
+if (technologyFaqSlider) {
+  /** @type {HTMLDivElement} */
+  const arrow = technologyFaqSlider.querySelector(".slider-arrow");
+
+  const swiper = new Swiper(technologyFaqSlider, {
+    modules: [Keyboard, Navigation],
+    keyboard: {
+      enabled: true,
+      pageUpDown: false,
+    },
+    navigation: {
+      enabled: true,
+      nextEl: arrow,
+    },
+    spaceBetween: 25,
+    rewind: true,
+  });
+}
