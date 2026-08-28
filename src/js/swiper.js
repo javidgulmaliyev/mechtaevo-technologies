c823fb59f1bd0b5454855c7c1eb719929cc49a5d
Javid Gulmaliyev <cavidgulmaliyev491995@gmail.com>
2026-08-28 15:42:21 +0400

    Add SCSS styles for technologies standards section and slider components
    - Created _standards.scss file to define styles for the technologies standards section.
    - Implemented responsive padding, font styles, and layout for the standards title and cards.
    - Added styles for the standards slider, including arrow positioning and card layout.
    - Ensured compatibility with various screen sizes using media queries and responsive functions.

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -44,3 +44,39 @@ if (technologyFaqSlider) {
     rewind: true,
   });
 }
+
+/** @type {HTMLDivElement} */
+const technologiesStandardsSlider = document.querySelector(".technologies-standards-slider");
+
+if (technologiesStandardsSlider) {
+  /** @type {HTMLDivElement} */
+  const arrow = technologiesStandardsSlider.querySelector(".slider-arrow");
+
+  const swiper = new Swiper(technologiesStandardsSlider, {
+    modules: [Keyboard, Navigation],
+    keyboard: {
+      enabled: true,
+      pageUpDown: false,
+    },
+    navigation: {
+      enabled: true,
+      nextEl: arrow,
+    },
+    breakpoints: {
+      "768.1": {
+        slidesPerView: 2,
+      },
+      "1440.1": {
+        slidesPerView: 3,
+      },
+    },
+    on: {
+      resize: (swiper) => {
+        swiper.params.spaceBetween = Math.min(Math.max(25, 25 + 50 * ((window.innerWidth - 1440) / 410)), 75);
+      }
+    },
+    slidesPerView: 1,
+    spaceBetween: Math.min(Math.max(25, 25 + 50 * ((window.innerWidth - 1440) / 410)), 75),
+    rewind: true,
+  });
+}
